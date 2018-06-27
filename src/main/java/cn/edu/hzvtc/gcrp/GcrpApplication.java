a04a67e03173bd43c853b6c45b14186b282674d3
tait4198 <tait4198@gmail.com>
2018-06-27 09:43:36 +0800

    init

---
+++ src/main/java/cn/edu/hzvtc/gcrp/GcrpApplication.java
@@ -0,0 +1,16 @@
+package cn.edu.hzvtc.gcrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * 程序入口
+ * @author tao
+ */
+@SpringBootApplication
+public class GcrpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcrpApplication.class, args);
+    }
+}
